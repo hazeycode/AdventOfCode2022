2f11b2599413719d6ba7edd7d1acb250b0306dd0
Chris Heyes <22148308+hazeycode@users.noreply.github.com>
2022-12-01 17:51:27 +0000

    day01 part two complete

--- src/day01.zig
+++ src/day01.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const parseInt = std.fmt.parseInt;
 
+const quicksort = @import("util/sort.zig").quicksort;
+
 var stdout = std.io.bufferedWriter(std.io.getStdOut().writer());
 
 fn println(comptime fmt: []const u8, args: anytype) void {
@@ -30,9 +32,42 @@ fn partOne(input_reader: anytype) !usize {
     return largest_acc;
 }
 
+fn partTwo(input_reader: anytype) !usize {
+    var largest_accs = [_]usize{0} ** 3;
+    var acc: usize = 0;
+
+    var buf = [_]u8{0} ** 16;
+    while (try input_reader.readUntilDelimiterOrEof(buf[0..], '\n')) |line| {
+        if (line.len == 0) {
+            for (largest_accs) |*p| {
+                if (acc >= p.*) {
+                    p.* = acc;
+                    quicksort(&largest_accs, 0, largest_accs.len - 1, struct {
+                        pub fn compare(_: @This(), a: usize, b: usize) bool {
+                            return a < b;
+                        }
+                    }{});
+                    break;
+                }
+            }
+            acc = 0;
+            continue;
+        }
+
+        acc += try parseInt(usize, line, 10);
+    }
+
+    return largest_accs[0] + largest_accs[1] + largest_accs[2];
+}
+
 pub fn main() !void {
     var input_stream = std.io.fixedBufferStream(@embedFile("data/day01.txt"));
 
     const part_one_answer = try partOne(input_stream.reader());
     println("part one answer = {}", .{part_one_answer});
+
+    input_stream.reset();
+
+    const part_two_answer = try partTwo(input_stream.reader());
+    println("part two answer = {}", .{part_two_answer});
 }
---
+++ src/util/sort.zig
@@ -0,0 +1,64 @@
+pub fn quicksort(
+    values: anytype,
+    low: isize,
+    high: isize,
+    comparitor: anytype,
+) void {
+    if (low >= high) return;
+    const p = partition(values, low, high, comparitor);
+    quicksort(values, low, p - 1, comparitor);
+    quicksort(values, p + 1, high, comparitor);
+}
+
+fn partition(
+    values: anytype,
+    low: isize,
+    high: isize,
+    comparitor: anytype,
+) isize {
+    const pivot = values[@intCast(usize, high)];
+    var i = low - 1;
+    var j = @intCast(usize, low);
+    while (j < high) : (j += 1) {
+        if (comparitor.compare(values[j], pivot)) {
+            i += 1;
+            swap(values, @intCast(usize, i), j);
+        }
+    }
+    i += 1;
+    swap(values, @intCast(usize, i), @intCast(usize, high));
+    return i;
+}
+
+fn swap(values: anytype, i: usize, j: usize) void {
+    const temp = values[i];
+    values[i] = values[j];
+    values[j] = temp;
+}
+
+const testing = @import("std").testing;
+
+test "quicksort" {
+    var comparitor = struct {
+        pub fn compare(_: @This(), a: u32, b: u32) bool {
+            return a < b;
+        }
+    }{};
+
+    {
+        var values = [_]u32{ 3, 7, 4, 234, 4, 19, 19 };
+        quicksort(&values, 0, values.len - 1, comparitor);
+
+        const expected = [_]u32{ 3, 4, 4, 7, 19, 19, 234 };
+
+        try testing.expectEqualSlices(u32, &expected, &values);
+    }
+    {
+        var values = [_]u32{ 3, 3 };
+        quicksort(&values, 0, values.len - 1, comparitor);
+
+        const expected = [_]u32{ 3, 3 };
+
+        try testing.expectEqualSlices(u32, &expected, &values);
+    }
+}
