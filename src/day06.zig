0f92f276fe297222e063119e13b2dc500d360198
Chris Heyes <22148308+hazeycode@users.noreply.github.com>
2022-12-06 10:10:24 +0000

    day06 part one complete

---
+++ src/day06.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const testing = std.testing;
+const assert = std.debug.assert;
+const fixedBufferStream = std.io.fixedBufferStream;
+
+const util = @import("util/util.zig");
+const println = util.println;
+
+const Bitset = std.StaticBitSet(256);
+
+fn partOne(stream: anytype) !usize {
+    const reader = stream.reader();
+
+    var count: usize = 0;
+
+    var buf = [_]u8{0} ** 4;
+    var cur_write: usize = 0;
+
+    while (true) {
+        const byte = reader.readByte() catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+
+        buf[cur_write] = byte;
+        cur_write = @mod(cur_write + 1, buf.len);
+
+        count += 1;
+
+        var bitset = Bitset.initEmpty();
+        for (buf) |c| bitset.set(c);
+
+        if (count >= buf.len and bitset.count() == buf.len) {
+            return count;
+        }
+    }
+
+    return error.NoMarkerFound;
+}
+
+pub fn main() !void {
+    var input_stream = fixedBufferStream(@embedFile("data/day06.txt"));
+
+    const part_one_answer = try partOne(&input_stream);
+    println("part one answer = {}", .{part_one_answer});
+
+    // input_stream.reset();
+
+    // const part_two_answer = try partTwo(&input_stream);
+    // println("part two answer = {}", .{part_two_answer});
+}
