65abfc41b9c7c928cd344f6c39d033e3f565baf7
Chris Heyes <22148308+hazeycode@users.noreply.github.com>
2022-12-09 15:59:52 +0000

    day09 part two: Passing test case but producing wrong answer on real data

--- src/day09.zig
+++ src/day09.zig
@@ -12,7 +12,7 @@ const Location = struct {
     y: i32 = 0,
 };
 
-const TailHistory = std.AutoHashMap(Location, void);
+const Visited = std.AutoHashMap(Location, void);
 
 fn parseLine(line: []const u8) struct { dir: Dir, dist: u16 } {
     var it = tokenize(u8, line, " ");
@@ -30,13 +30,39 @@ fn parseLine(line: []const u8) struct { dir: Dir, dist: u16 } {
     };
 }
 
-fn recordTailHistory(allocator: std.mem.Allocator, input: []const u8) !TailHistory {
-    var result = TailHistory.init(allocator);
+fn updateKnot(knot: *Location, prev_knot: *const Location) bool {
+    const dx = prev_knot.x - knot.x;
+    const dy = prev_knot.y - knot.y;
+
+    const absx = if (dx < 0) -dx else dx;
+    const absy = if (dy < 0) -dy else dy;
+    if (absx <= 1 and absy <= 1) {
+        return false;
+    }
+
+    if (knot.y == prev_knot.y) {
+        knot.x += if (dx < 0) -1 else 1;
+    } else if (knot.x == prev_knot.x) {
+        knot.y += if (dy < 0) -1 else 1;
+    } else {
+        knot.x += if (dx < 0) -1 else 1;
+        knot.y += if (dy < 0) -1 else 1;
+    }
+
+    return true;
+}
+
+fn recordTailHistory(
+    comptime tail_len: usize,
+    input: []const u8,
+    history: *Visited,
+) !void {
+    comptime assert(tail_len > 0);
 
     var head = Location{};
-    var tail = Location{};
+    var tail = [_]Location{.{}} ** tail_len;
 
-    try result.put(tail, {});
+    try history.put(tail[tail.len - 1], {});
 
     var it = tokenize(u8, input, "\n");
     while (it.next()) |line| {
@@ -51,31 +77,18 @@ fn recordTailHistory(allocator: std.mem.Allocator, input: []const u8) !TailHisto
                 .left => head.x -= 1,
             }
 
-            { // update tail
-                const dx = head.x - tail.x;
-                const dy = head.y - tail.y;
-
-                const absx = if (dx < 0) -dx else dx;
-                const absy = if (dy < 0) -dy else dy;
-                if (absx <= 1 and absy <= 1) {
-                    continue;
+            if (updateKnot(&tail[0], &head)) {
+                var j: usize = 1;
+                while (j < tail.len) : (j += 1) {
+                    if (updateKnot(&tail[j], &tail[j - 1]) == false) {
+                        break;
+                    }
                 }
-
-                if (tail.y == head.y) {
-                    tail.x += if (dx < 0) -1 else 1;
-                } else if (tail.x == head.x) {
-                    tail.y += if (dy < 0) -1 else 1;
-                } else {
-                    tail.x += if (dx < 0) -1 else 1;
-                    tail.y += if (dy < 0) -1 else 1;
-                }
-
-                try result.put(tail, {});
             }
+
+            try history.put(tail[tail.len - 1], {});
         }
     }
-
-    return result;
 }
 
 pub fn main() !void {
@@ -84,11 +97,20 @@ pub fn main() !void {
     const data = @embedFile("data/day09.txt");
 
     var allocator = std.heap.page_allocator;
-
-    var tail_history = try recordTailHistory(allocator, data);
+    var tail_history = Visited.init(allocator);
     defer tail_history.deinit();
 
-    println("part one answer: {}", .{tail_history.count()});
+    { // part one
+        try recordTailHistory(1, data, &tail_history);
+        println("part one answer: {}", .{tail_history.count()});
+    }
+
+    tail_history.clearRetainingCapacity();
+
+    { // part two
+        try recordTailHistory(10, data, &tail_history);
+        println("part two answer: {}", .{tail_history.count()});
+    }
 }
 
 test {
@@ -103,8 +125,47 @@ test {
         \\R 2
     ;
 
-    var tail_history = try recordTailHistory(testing.allocator, data);
+    var tail_history = Visited.init(testing.allocator);
     defer tail_history.deinit();
 
+    try recordTailHistory(1, data, &tail_history);
     try testing.expectEqual(@as(usize, 13), tail_history.count());
+}
+
+test {
+    const data =
+        \\R 4
+        \\U 4
+        \\L 3
+        \\D 1
+        \\R 4
+        \\D 1
+        \\L 5
+        \\R 2
+    ;
+
+    var tail_history = Visited.init(testing.allocator);
+    defer tail_history.deinit();
+
+    try recordTailHistory(9, data, &tail_history);
+    try testing.expectEqual(@as(usize, 1), tail_history.count());
+}
+
+test {
+    const data =
+        \\R 5
+        \\U 8
+        \\L 8
+        \\D 3
+        \\R 17
+        \\D 10
+        \\L 25
+        \\U 20
+    ;
+
+    var tail_history = Visited.init(testing.allocator);
+    defer tail_history.deinit();
+
+    try recordTailHistory(9, data, &tail_history);
+    try testing.expectEqual(@as(usize, 36), tail_history.count());
 }
